4dcb0f713c1652bc69fe41f68fbfb1f872a6af73
Mohammad Arkan <mdarkanurl@gmail.com>
2025-10-16 18:08:38 +0600

    Almost drizzle set up done

---
+++ src/database/schema.ts
@@ -0,0 +1,12 @@
+import { integer, text, boolean, pgTable, uuid } from "drizzle-orm/pg-core";
+
+export const startup = pgTable("startup", {
+  id: uuid("id").primaryKey(),
+  name: text("name").notNull(),
+  website: boolean("website").notNull(),
+  description: text("description").notNull(),
+  VC_firm: text("VC_firm").notNull(),
+  services: text("services").notNull(),
+  foundedAt: text("foundedAt"),
+  Headquarters: text("Headquarters")
+});
