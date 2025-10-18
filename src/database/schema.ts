1902be5eff1985c74f4a4afa0cea95c2eba96a0f
Mohammad Arkan <mdarkanurl@gmail.com>
2025-10-18 09:42:57 +0600

    Fixed a bug

--- src/database/schema.ts
+++ src/database/schema.ts
@@ -3,7 +3,7 @@ import { integer, text, boolean, pgTable, uuid } from "drizzle-orm/pg-core";
 export const startup = pgTable("startup", {
   id: uuid("id").primaryKey(),
   name: text("name").notNull(),
-  website: boolean("website").notNull(),
+  website: text("website").notNull(),
   description: text("description").notNull(),
   VC_firm: text("VC_firm").notNull(),
   services: text("services").notNull(),
