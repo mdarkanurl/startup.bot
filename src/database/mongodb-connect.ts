9478123d173d897a1129f2fcf34d5f4ebb999daf
Mohammad Arkan <mdarkanurl@gmail.com>
2025-10-25 10:49:45 +0600

    Made MongoDB connection function more reliable and robust

--- src/database/mongodb-connect.ts
+++ src/database/mongodb-connect.ts
@@ -1,18 +1,25 @@
 import mongoose from "mongoose";
-import { MONGODB_CONNECT_URL } from "../config";
+import { config } from "dotenv";
+config();
+
+const MONGODB_CONNECT_URL = process.env.MONGODB_CONNECT_URL || "mongodb://127.0.0.1:27017/startup.bot";
 
 const DBConnect = async () => {
   try {
     await mongoose.connect(MONGODB_CONNECT_URL);
     console.log("MongoDB is connected");
+
+    mongoose.connection.on("disconnected", () => {
+      console.log("MongoDB is disconnected!");
+    });
+
+    mongoose.connection.on("error", (err) => {
+      console.error("MongoDB error:", err);
+    });
   } catch (error: any) {
     console.log("MongoDB failed to connect");
     console.log(error.message);
   }
 };
 
-DBConnect();
-
-export {
-    DBConnect
-}
+export { DBConnect };
