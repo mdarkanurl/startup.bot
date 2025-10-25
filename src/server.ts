90ff60d32b4c48cf1228d2a1d2ed2b832a835cd0
Mohammad Arkan <mdarkanurl@gmail.com>
2025-10-25 21:35:11 +0600

    Added crawlee function testing

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import app from "./app";
 import { MongoDB } from "./database";
 import { config } from "dotenv";
+import { main } from "./modules/fetch-startup-data/crawlee";
 config();
 
 const PORT = process.env.PORT || 404;
@@ -9,4 +10,5 @@ app.listen(PORT, async () => {
     console.log("server is running at port", PORT);
     console.log(`Here's the endpoint http://localhost:${PORT}`);
     await MongoDB.DBConnect();
+    await main();
 });
