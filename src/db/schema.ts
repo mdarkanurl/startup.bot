7f5b179dab407c0c884231b3bb28b5f6ed56aa46
Mohammad Arkan <mdarkanurl@gmail.com>
2025-11-01 09:22:41 +0600

    redesign schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,28 +1,34 @@
+import { relations } from "drizzle-orm";
 import { text, pgTable, uuid, boolean } from "drizzle-orm/pg-core";
 
-export const startup = pgTable("startup", {
-  id: uuid("id").primaryKey(),
-  name: text("name").notNull(),
-  website: text("website").notNull(),
-  description: text("description").notNull(),
-  VC_firm: text("VC_firm").notNull(),
-  services: text("services").notNull(),
-  founder_names: text("founder_names").array().notNull(),
-  foundedAt: text("foundedAt"),
-  isUsed: boolean("isUsed").notNull().default(false),
-});
-
-
-export const raw_startup_data = pgTable("raw_startup_data", {
+export const web_page_data = pgTable("web_page_data", {
   id: uuid("id").primaryKey(),
   url: text("url").notNull(),
   title: text("title").notNull(),
   description: text("description").notNull(),
   text: text("text").notNull(),
   crawledAt: text("crawledAt").notNull(),
-  VC_firm: text("VC_firm").notNull().default("unknown"),
-  services: text("services").notNull().default(""),
-  founder_names: text("founder_names").array().notNull().default([]),
-  foundedAt: text("foundedAt"),
   isUsed: boolean("isUsed").notNull().default(false),
 });
+
+export const startup = pgTable("startup", {
+  id: uuid("id").primaryKey(),
+  name: text("name").notNull(),
+  VC_firm: text("VC_firm").notNull().default("unknown"),
+  founder_names: text("founder_names[]").array().notNull().default([]),
+  foundedAt: text("foundedAt"),
+  web_page_data_id: uuid("web_page_data_id")
+    .references(() => web_page_data.id)
+    .notNull(),
+});
+
+export const startupRelations = relations(startup, ({ one }) => ({
+  web_page_data: one(web_page_data, {
+    fields: [startup.web_page_data_id],
+    references: [web_page_data.id],
+  }),
+}));
+
+export const webPageDataRelations = relations(web_page_data, ({ many }) => ({
+  startups: many(startup),
+}));
