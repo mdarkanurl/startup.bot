53ce12a6b4d90e5b5e66d566c2999250c80cd8db
Mohammad Arkan <mdarkanurl@gmail.com>
2025-11-01 09:59:44 +0600

    Changed schema as needed

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -2,29 +2,28 @@ import { relations } from "drizzle-orm";
 import { text, pgTable, uuid, boolean } from "drizzle-orm/pg-core";
 
 export const web_page_data = pgTable("web_page_data", {
-  id: uuid("id").primaryKey(),
+  id: uuid("id").defaultRandom().primaryKey(),
   url: text("url").notNull(),
   title: text("title").notNull(),
   description: text("description").notNull(),
   text: text("text").notNull(),
-  crawledAt: text("crawledAt").notNull(),
   isUsed: boolean("isUsed").notNull().default(false),
 });
 
 export const startup = pgTable("startup", {
-  id: uuid("id").primaryKey(),
+  id: uuid("id").defaultRandom().primaryKey(),
   name: text("name").notNull(),
   VC_firm: text("VC_firm").notNull().default("unknown"),
   founder_names: text("founder_names[]").array().notNull().default([]),
   foundedAt: text("foundedAt"),
-  web_page_data_id: uuid("web_page_data_id")
-    .references(() => web_page_data.id)
-    .notNull(),
+  web_page_data_ids: uuid("web_page_data_id")
+    .array()
+    .references(() => web_page_data.id),
 });
 
 export const startupRelations = relations(startup, ({ one }) => ({
   web_page_data: one(web_page_data, {
-    fields: [startup.web_page_data_id],
+    fields: [startup.web_page_data_ids],
     references: [web_page_data.id],
   }),
 }));
