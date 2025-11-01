1aa78f05b9ff4dbe96c91017b26972cca488ced8
Mohammad Arkan <mdarkanurl@gmail.com>
2025-11-01 09:59:18 +0600

    Makr variable & path more correct

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,11 +1,18 @@
 import { Startup } from "./mongodb/mongodb";
 import { DBConnect } from "./mongodb/mongodb-connect";
+import { startup, web_page_data } from "./schema";
 
 const MongoDB = {
     StartupDB: Startup,
     DBConnect
 }
 
+const Tables = {
+    startup,
+    web_page_data
+}
+
 export {
-    MongoDB
+    MongoDB,
+    Tables
 }
