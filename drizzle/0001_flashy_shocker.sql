1514c1b157543f38c45e444373f0136be3535d3c
Mohammad Arkan <mdarkanurl@gmail.com>
2025-11-01 09:22:07 +0600

    Removed and added migrate files

--- drizzle/0001_flashy_shocker.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE "raw_startup_data" (
-	"id" uuid PRIMARY KEY NOT NULL,
-	"url" text NOT NULL,
-	"title" text NOT NULL,
-	"description" text NOT NULL,
-	"text" text NOT NULL,
-	"crawledAt" text NOT NULL
-);
