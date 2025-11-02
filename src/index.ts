c6d377716537ae42fc7d65ce29dd50b2122b82f3
Mohammad Arkan <mdarkanurl@gmail.com>
2025-11-02 21:54:07 +0600

    comment some code

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,19 @@
 import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/node-postgres';
-import { fetchYCombinatorStartups, startups } from './modules/fetch-startup-data/YCombinator/ycombinator';
-import { ai2incubator } from './modules/fetch-startup-data/ai2incubator';
+// import { fetchYCombinatorStartups, startups } from './modules/fetch-startup-data/YCombinator/ycombinator';
+// import { ai2incubator } from './modules/fetch-startup-data/ai2incubator';
 
 const db = drizzle(process.env.DATABASE_URL!);
 
 // crawl VC websites or other sources and get startups data
-const startupsData = async () => {
-    await fetchYCombinatorStartups(startups); // YCombinator
-    await ai2incubator(); // AI2 Incubator
-}
+// const startupsData = async () => {
+//     await fetchYCombinatorStartups(startups); // YCombinator
+//     await ai2incubator(); // AI2 Incubator
+// }
 
 // generate tweets and save them to the database
 
 export {
     db,
-    startupsData,
+    // startupsData,
 }
