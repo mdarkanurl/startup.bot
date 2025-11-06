f4b14dfc677ff14a66f329cf53dc68e539cd69d9
Mohammad Arkan <mdarkanurl@gmail.com>
2025-11-06 21:30:45 +0600

    Added logic for fetch data fron postgresql DB

--- src/modules/ai-research/genreate-tweets.ts
+++ src/modules/ai-research/genreate-tweets.ts
@@ -1,9 +1,26 @@
 import { db } from "../../index";
+import { sql } from 'drizzle-orm';
 
 
 const generateAndSaveTweets = async () => {
     // Get startup data from the database
-    // const startups = await db.startups.findMany();
+    const pages = await db.query.web_page_data.findMany({
+        where: (web_page_data, { eq, and }) =>
+            and(
+            eq(web_page_data.isUsed, false),
+            eq(
+                web_page_data.startupId,
+                sql`(
+                SELECT "startupId" FROM "web_page_data"
+                WHERE "isUsed" = false
+                LIMIT 1
+                )`
+            )
+        ),
+    });
+
+    console.log('Pages to generate tweets for:', pages);
+
 
     // format the data for tweet generation
 
@@ -11,3 +28,5 @@ const generateAndSaveTweets = async () => {
 
     // Save the generated tweets back to the database
 }
+
+generateAndSaveTweets();
