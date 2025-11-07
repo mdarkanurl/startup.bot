b89817b478cd18ba6af6481653e59a536f5ba558
Mohammad Arkan <mdarkanurl@gmail.com>
2025-11-07 09:40:28 +0600

    Remove database connection

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,8 @@
 import 'dotenv/config';
-import { drizzle } from 'drizzle-orm/node-postgres';
 import { getStartupDataFromWebsite, consumerEvents } from './modules/fetch-startup-data/crawlee';
-import { Tables } from "./db";
+
 // import { fetchYCombinatorStartups, startups } from './modules/fetch-startup-data/YCombinator/ycombinator';
 // import { ai2incubator } from './modules/fetch-startup-data/ai2incubator';
-
-const db = drizzle(process.env.DATABASE_URL!, { schema: Tables });
 
 // crawl VC websites or other sources and get startups data
 // const startupsData = async () => {
@@ -19,6 +16,5 @@ consumerEvents.on('pageCrawled', async () => await getStartupDataFromWebsite());
 // generate tweets and save them to the database
 
 export {
-    db,
     // startupsData,
 }
