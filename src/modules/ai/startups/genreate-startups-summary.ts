0ae2cfef8cabdb4269eda7344356ddda15631698
Mohammad Arkan <mdarkanurl@gmail.com>
2025-11-09 21:33:25 +0600

    Added code for update isUsed in DB

--- src/modules/ai/startups/genreate-startups-summary.ts
+++ src/modules/ai/startups/genreate-startups-summary.ts
@@ -2,6 +2,8 @@ import { db } from "../../../connection";
 import { redis } from "../../../db/redis";
 import 'dotenv/config';
 import { generateSummaryOfPages } from "./generate-summary-of-pages";
+import { web_page_data } from "../../../db/schema";
+import { eq } from "drizzle-orm";
 
 const CHUNK_SIZE = Number(process.env.CHUNK_SIZE) || 5;
 
@@ -54,6 +56,17 @@ export async function generateSummaryOfStartups() {
         console.log("✅ Summaries saved to Redis:", saveSummary);
     } catch (error) {
         console.error("❌ Error saving summaries to Redis:", error);
+    }
+
+    try {
+        // Update database to mark pages as used
+        await db.update(web_page_data)
+            .set({ isUsed: true })
+            .where(eq(web_page_data.startupId, pages[0].startupId));
+
+        console.log("✅ Pages isUsed is updated in database");
+    } catch (error) {
+        console.error("❌ Error updating pages in database:", error);
     }
 
     return summaries;
