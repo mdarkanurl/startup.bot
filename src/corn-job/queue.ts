03ef81b6e067e31e5a84092499375da95e017eca
Mohammad Arkan <mdarkanurl@gmail.com>
2025-11-24 22:13:41 +0600

    refactor: remove redundant job files and unused code

--- src/corn-job/queue.ts
+++
@@ -1,4 +0,0 @@
-import { Queue } from "bullmq";
-import { redis } from "../db/redis/index";
-
-export const jobsQueue = new Queue("jobs-queue", { connection: redis });
--- src/db/redis/connection.ts
+++ src/db/redis/connection.ts
@@ -2,7 +2,8 @@ import Redis from "ioredis";
 
 const redis = new Redis({
     host: process.env.REDIS_HOST || "localhost",
-    port: Number(process.env.REDIS_PORT) || 6379
+    port: Number(process.env.REDIS_PORT) || 6379,
+    maxRetriesPerRequest: null
 });
 
 export { redis };
