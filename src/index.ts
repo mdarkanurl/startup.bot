045d777f97da0f4655416451d3af53e0d1561153
Mohammad Arkan <mdarkanurl@gmail.com>
2025-11-25 22:35:31 +0600

    refactor: optimize startup information retrieval and publishing workflow

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,44 @@
+import { fetchTodayYesterdayPosts } from "./modules/fetch-data-from-online/collect-startups-info/product-hunt";
 import { aiUtils } from "./utils/ai-utils";
+import { getStartupDataFromWebsite } from "./modules/fetch-data-from-online/website-crawler/crawlee";
+import { ai_startups } from "./modules/ai/startups";
+import { generateTweet } from "./modules/ai/tweet/generate-tweet";
+import { generateBlog } from "./modules/ai/blog/generate-blog";
+import { postTweet } from "./modules/ai/tweet/post-tweet";
+import { postBlog } from "./modules/ai/blog/post-blog";
 
 async function main() {
     try {
-        let now: Date = new Date(); // 2025-11-25T06:46:42.509Z
-        let oneDayLater: Date = new Date(now.getTime() + (24 * 60 * 60 * 1000)); // 2025-11-26T06:46:42.509Z
-        let oneHoursLater: Date = new Date(now.getTime() + (1 * 60 * 60 * 1000)); // 2025-11-25T07:46:42.509Z
-        // await getStartupsinfo();
+        let now: Date = new Date();
+        let oneDayLater: Date = new Date(now.getTime() + (24 * 60 * 60 * 1000));
+        let oneHoursLater: Date = new Date(now.getTime() + (1 * 60 * 60 * 1000));
+        await fetchTodayYesterdayPosts();
 
         while (true) {
             now = new Date();
-            // await crawlWebsite();
+            await getStartupDataFromWebsite();
             await aiUtils.delay(60000);
 
-            // await generateSummaryOfStartups();
+            await ai_startups.generateSummaryOfStartups();
             await aiUtils.delay(60000);
 
-            // await generateTweet()
+            await generateTweet();
             await aiUtils.delay(60000);
 
-            // await genertaeBlog();
+            await generateBlog();
             await aiUtils.delay(60000);
 
             if(now >= oneHoursLater) {
-                // await postTweet();
+                await postTweet();
                 oneHoursLater = new Date(now.getTime() + (1 * 60 * 60 * 1000));
                 await aiUtils.delay(60000);
             }
 
             if(now >= oneDayLater) {
-                // await getStartupsinfo();
+                await postBlog();
                 await aiUtils.delay(60000);
-                // await postBlog();
+                
+                await fetchTodayYesterdayPosts();
                 oneDayLater = new Date(now.getTime() + (24 * 60 * 60 * 1000));
                 await aiUtils.delay(60000);
             };
